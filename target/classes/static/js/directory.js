fa0920495f3d37274bab9b284108911194848200
Your Name <you@example.com>
2024-07-29 12:30:25 -0300

    Directory's modals done!

--- target/classes/static/js/directory.js
+++ target/classes/static/js/directory.js
@@ -18,7 +18,46 @@ for(let i=0;i < fileItems.length; i++){
 
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const folderItems = document.getElementsByClassName("folderItem");
 
+    for (let i = 0; i < folderItems.length; i++) {
+        folderItems[i].addEventListener('dblclick', function() {
+            const directoryId = this.getAttribute("data-id");
+            window.location.href = "/directory/" + directoryId;
+        });
+
+        folderItems[i].addEventListener('contextmenu', function(event) {
+            event.preventDefault(); // Previne o menu de contexto padrão do navegador
+            const contextMenu = document.getElementById('context-menu');
+            
+            // Posiciona o menu de contexto
+            contextMenu.style.top = `${event.clientY}px`;
+            contextMenu.style.left = `${event.clientX}px`;
+            contextMenu.style.display = 'block';
+            
+            // Armazena o ID do item clicado
+            contextMenu.setAttribute('data-id', this.getAttribute("data-id"));
+        });
+    }
+
+    // Fecha o menu de contexto quando clicar fora
+    document.addEventListener('click', function(event) {
+        const contextMenu = document.getElementById('context-menu');
+        if (!contextMenu.contains(event.target)) {
+            contextMenu.style.display = 'none';
+        }
+    });
+
+    // Alterna o tema
+    const toggleThemeButton = document.getElementById("theme-checkbox");
+    const body = document.body;
+    const mainElement = document.querySelector('main');
+    const headerElement = document.querySelector('header');
+    const navElement = document.querySelector('nav');
+
+
+});
 
 
 
