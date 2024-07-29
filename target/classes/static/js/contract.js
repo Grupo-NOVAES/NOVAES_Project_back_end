473a180dd0dc6267071187a387496f63006622e6
Brxnin-FX <brunosantiago7002@gmail.com>
2024-07-29 08:38:40 -0300

    repair 2

--- target/classes/static/js/contract.js
+++
@@ -1,8 +0,0 @@
-const folderItems = document.getElementsByClassName("contractItem");
-
-for (let i = 0; i < folderItems.length; i++) {
-    folderItems[i].addEventListener('dblclick', function() {
-        const contractId = this.getAttribute("data-id");
-        window.location.href = "/stages/" + contractId;
-    });
-}
