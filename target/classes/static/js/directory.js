319955d185621c1f8594f7d5ae4b2aceaab426b0
Your Name <you@example.com>
2024-07-29 12:20:30 -0300

    Add Folder's Modal done

--- target/classes/static/js/directory.js
+++ target/classes/static/js/directory.js
@@ -15,3 +15,54 @@ for(let i=0;i < fileItems.length; i++){
         window.location.href = "/archive/download/"+fileId;
     })
 }
+
+
+
+
+
+
+
+
+
+function showModal() {
+    document.getElementById('AddFolderModal').style.display = 'block';
+}
+
+function hideModalAddFolder() {
+    document.getElementById('AddFolderModal').style.display = 'none';
+}
+
+function saveButtonAddFolder() {
+    console.log('Salvar Pasta');
+    hideModal();
+}
+
+
+
+function showModalFile() {
+    document.getElementById('AddFolderModalFile').style.display = 'block';
+}
+
+function hideModalFile() {
+    document.getElementById('AddFolderModalFile').style.display = 'none';
+}
+
+function saveButtonFile() {
+    console.log('Salvar arquivo');
+    hideModalFile();
+}
+
+
+
+function showModalFolderName() {
+    document.getElementById('EditModalFolder').style.display = "block";
+}
+
+function hideModalEdit() {
+    document.getElementById('EditModalFolder').style.display = "none";
+}
+
+function saveButtonEditFolder () {
+    console.log('Salvar Edição Pasta!')
+    hideModalEdit();
+}
