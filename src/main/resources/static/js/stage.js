0d4351286971cced96e92e9410610f9332ed7f8a
Your Name <you@example.com>
2024-07-30 11:30:40 -0300

    Add Modal Stage's ready

---
+++ src/main/resources/static/js/stage.js
@@ -0,0 +1,11 @@
+function showModalStage(){
+    document.getElementById('AddModalStage').style.display = "block";
+}
+
+function hideButton() {
+    document.getElementById('AddModalStage').style.display = "none";
+}
+
+function saveButton() {
+    console.log('Salvar Add Modal Stage!');
+}
