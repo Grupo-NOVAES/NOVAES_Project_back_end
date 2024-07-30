7e5ef4b8f8f483ba68e39b9f731c298c987b0441
Your Name <you@example.com>
2024-07-30 09:33:22 -0300

    Add Modal Contract's ready!

--- src/main/resources/static/js/contract.js
+++ src/main/resources/static/js/contract.js
@@ -6,3 +6,16 @@ for (let i = 0; i < folderItems.length; i++) {
         window.location.href = "/stages/" + contractId;
     });
 }
+
+
+function showModalContract() {
+    document.getElementById('AddModalContract').style.display = "block";
+}
+
+function hideModalContract() {
+    document.getElementById('AddModalContract').style.display = "none";
+}
+
+function saveButton(){
+    console.log('Salvar Add Contrato!');
+}
