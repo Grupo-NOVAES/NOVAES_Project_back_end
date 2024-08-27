8a75e6478119e8900421e4a355921c09c1cc82a1
Brxnin-FX <brunosantiago7002@gmail.com>
2024-08-27 10:43:06 -0300

    refactor database init

--- src/main/java/com/app/novaes/util/DatabaseInit.java
+++ src/main/java/com/app/novaes/util/DatabaseInit.java
@@ -45,14 +45,13 @@ public class DatabaseInit implements CommandLineRunner{
 	}
 	
 	public void insertUserAdmin() {
-		Client client = new Client();
         if(employeeRepository.findByLogin("admin@gmail.com") == null) {
         	Employee admin = new Employee();
-        	admin.setName("Novaes");
-        	admin.setLastname("ADM");
-        	admin.setLogin("admin@gmail.com");
-        	admin.setPassword(passwordEncoder.encode("123456"));
-        	admin.setPhoneNumber("(16) 99999-9999");
+        	admin.setName("Admin");
+        	admin.setLastname("Novaes");
+        	admin.setLogin("adminNovaes@gmail.com");
+        	admin.setPassword(passwordEncoder.encode("novaes2011"));
+        	admin.setPhoneNumber("(16) 3419-0906");
         	admin.setEnabled(true);
         	admin.setRole(Role.ADMIN);
         	admin.setOffice("Gerente");
@@ -60,78 +59,13 @@ public class DatabaseInit implements CommandLineRunner{
 
             employeeRepository.save(admin);
         }
-        if(employeeRepository.findByLogin("gerente@novaes.com") == null) {
-        	Employee gerente = new Employee();
-        	gerente.setName("Novaes");
-        	gerente.setLastname("Employee");
-        	gerente.setLogin("gerente@novaes.com");
-        	gerente.setPassword(passwordEncoder.encode("123456"));
-        	gerente.setPhoneNumber("(16) 99999-9999");
-        	gerente.setEnabled(true);
-        	gerente.setRole(Role.ADMIN);
-        	gerente.setOffice("Funcionario");
-        	gerente.setAdmin(true);
-
-            employeeRepository.save(gerente);
-        }
-        if(employeeRepository.findByLogin("eng@novaes.com") == null) {
-        	Employee employee = new Employee();
-        	employee.setName("Novaes");
-            employee.setLastname("Employee");
-            employee.setLogin("eng@novaes.com");
-            employee.setPassword(passwordEncoder.encode("123456"));
-            employee.setPhoneNumber("(16) 99999-9999");
-            employee.setEnabled(true);
-            employee.setRole(Role.EMPLOYEE);
-            employee.setOffice("Funcionario");
-            employee.setAdmin(false);
-
-            employeeRepository.save(employee);
-        }
-        if(clientRepository.findByLogin("client@novaes.com") == null) {
-        	client.setName("Agúas");
-        	client.setLastname("de Araçoiaba");
-        	client.setLogin("client@novaes.com");
-        	client.setPassword(passwordEncoder.encode("123456"));
-        	client.setPhoneNumber("(16) 99999-9999");
-        	client.setEnabled(true);
-        	client.setRole(Role.USER);
-        	client.setEntrerprise_name("Aguas de Araçoiaba");
-        	client.setReferences_directory((long) 3);
- 
-
-            clientRepository.save(client);
-        }
 	}
 	
 	public void inserRootDirectory() {
-		boolean ifRootNotExist = false;
 		if(directoryRepository.findByName("root") == null) {
 			Directory root = new Directory();
 			root.setName("root");
 			directoryRepository.save(root);
-			ifRootNotExist=true;
-		}
-		if(ifRootNotExist) {
-			Directory directory = new Directory();
-			directory.setName("Jau");
-			directory.setParentDirectory(directoryRepository.findByName("root"));
-			directoryRepository.save(directory);
-		}
-		if(ifRootNotExist) {
-			Directory directory = new Directory();
-			directory.setName("Aguas de Aracoiaba");
-			directory.setParentDirectory(directoryRepository.findByName("root"));
-			directoryRepository.save(directory);
-		}
-		if(ifRootNotExist) {
-			Directory directory = new Directory();
-			directory.setName("Itu");
-			directory.setParentDirectory(directoryRepository.findByName("root"));
-			directoryRepository.save(directory);
 		}
 	}
-	
-	
-
 }
