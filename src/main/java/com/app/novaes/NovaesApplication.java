4d4ac2a09d3c9478b7f70b9dafd6a9c9d9e54d60
Brxnin-FX <brunosantiago7002@gmail.com>
2025-04-16 12:38:48 -0300

    test

--- src/main/java/com/app/novaes/NovaesApplication.java
+++ src/main/java/com/app/novaes/NovaesApplication.java
@@ -8,6 +8,7 @@ public class NovaesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovaesApplication.class, args);
+		System.out.println("=== Startup Completed ===");
 	}
 
 }
