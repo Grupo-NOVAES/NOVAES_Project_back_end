96cbff3351aba8c851beed1c142624e4d46eb44f
Brxnin-FX <brunosantiago7002@gmail.com>
2025-04-23 12:42:57 -0300

    teste

--- src/main/java/com/app/novaes/NovaesApplication.java
+++ src/main/java/com/app/novaes/NovaesApplication.java
@@ -1,18 +1,13 @@
 package com.app.novaes;
 
-import java.util.logging.Logger;
-
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NovaesApplication {
-    private static final Logger log = (Logger) LoggerFactory.getLogger(NovaesApplication.class);
-    
-    public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(NovaesApplication.class, args);
-        log.info("=== APPLICATION STARTED SUCCESSFULLY ===");  // Visible in logs
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaesApplication.class, args);
+	}
+
 }
